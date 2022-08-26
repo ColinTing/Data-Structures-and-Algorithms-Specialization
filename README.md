3d45bb7f4f94fa9aabcd88eb612a996d131ee8ab
ColinTing <honey.hao.2012@gmail.com>
2022-08-26 23:28:32 +0800

    week1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data Structures and Algorithms Specialization
+
+此项目为Coursera上"<a href="https://www.coursera.org/specializations/data-structures-algorithms">数据结构与算法 专项课程</a>"课后作业。
+科学的学习方法是通过不同场景下对知识应用达到对知识的掌握。这里通过听课，自己的刻意练习及完成课后作业来实践科学的学习方法。
---
+++ week1/2_maximum_pairwise_product/max_pairwise_product_fast.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long MaxPairwiseProduct(const vector<int>& numbers) {
+    int n = numbers.size();
+    
+    int max_index1 = -1;
+    
+    for (int first = 0; first < n; ++first) {
+       if ((max_index1 == -1) || (numbers[first] > numbers[max_index1])) {
+	 max_index1 = first;
+       } 
+    }
+	
+    int max_index2 = -1;
+
+    for (int second = 0; second < n; ++second) {
+    	if ((second != max_index1) && ((max_index2 == -1) || (numbers[second] > numbers[max_index2]))) {
+          max_index2 = second;
+        }
+    }
+//    cout << max_index1 << ' ' << max_index2 << "\n";
+    return ((long long)(numbers[max_index1])) * numbers[max_index2] ;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+    cout << MaxPairwiseProduct(numbers) << "\n";
+    return 0;
+}
